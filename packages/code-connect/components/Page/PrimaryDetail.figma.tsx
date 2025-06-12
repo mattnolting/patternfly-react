a563e299aa947db543767a772fdce7303d7fe606
Matt Nolting <matthew.nolting@gmail.com>
2025-06-12 10:08:11 -0400

    feat(CC-all-components): udpated datalist and page

--- packages/code-connect/components/Page/PrimaryDetail.figma.tsx
+++ packages/code-connect/components/Page/PrimaryDetail.figma.tsx
@@ -1,12 +1,6 @@
 import figma from '@figma/code-connect';
 import { Page } from '@patternfly/react-core';
 
-/**
- * PatternFly Page integration for Figma Code Connect
- * @patternfly https://www.patternfly.org/components/page
- * @figma https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=25656-311494
- */
-
 figma.connect(
   Page,
   'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=25656-311494&t=IzSunfrnw18ti37Y-11',
