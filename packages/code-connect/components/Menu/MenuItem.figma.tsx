8a34b25329c99bc0683852943f3366c4f10fd9e6
Matt Nolting <matthew.nolting@gmail.com>
2025-07-01 16:49:59 -0400

    feat(CC-all-components): menu updated

--- packages/code-connect/components/Menu/MenuItem.figma.tsx
+++ packages/code-connect/components/Menu/MenuItem.figma.tsx
@@ -1,5 +1,14 @@
 import figma from '@figma/code-connect';
-import { MenuItem, MenuItemAction, Divider, MenuFooter, MenuBreadcrumb } from '@patternfly/react-core';
+import {
+  Divider,
+  DrilldownMenu,
+  MenuItem,
+  MenuItemAction,
+  MenuFooter,
+  MenuBreadcrumb,
+  MenuList,
+  MenuGroup
+} from '@patternfly/react-core';
 import BellIcon from '@patternfly/react-icons/dist/esm/icons/bell-icon';
 import CodeBranchIcon from '@patternfly/react-icons/dist/esm/icons/code-branch-icon';
 
@@ -11,16 +20,6 @@ import CodeBranchIcon from '@patternfly/react-icons/dist/esm/icons/code-branch-i
 //   true: 'This is a description',
 //   false: undefined
 // }),
-
-// Documentation for MenuItem can be found at https://www.patternfly.org/components/menu
-
-const resetProps = {
-  reset: {
-    isExternalLink: undefined,
-    to: undefined,
-    itemId: undefined
-  }
-};
 
 figma.connect(
   MenuItem,
@@ -60,8 +59,42 @@ figma.connect(
   MenuItem,
   'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=2579-17546',
   {
+    variant: { 'Menu Item': 'Group Header' },
     props: {
-      ...resetProps,
+      children: figma.children('*'),
+      menuItemText: figma.string('Menu item Text')
+    },
+    example: (props) => (
+      <>
+        <MenuGroup label={props.menuItemText} labelHeadingLevel="h3">
+          <MenuList>
+            <MenuItem to="#" itemId={1}>
+              Link 1
+            </MenuItem>
+            <MenuItem itemId={2}>Link 2</MenuItem>
+          </MenuList>
+        </MenuGroup>
+        <Divider />
+        <MenuGroup label={props.menuItemText} labelHeadingLevel="h3">
+          <MenuList>
+            <MenuItem to="#" itemId={3}>
+              Link 1
+            </MenuItem>
+            <MenuItem to="#" itemId={4}>
+              Link 2
+            </MenuItem>
+          </MenuList>
+        </MenuGroup>
+      </>
+    )
+  }
+);
+
+figma.connect(
+  MenuItem,
+  'https://www.figma.com/design/aEBBvq0J3EPXxHvv6WgDx9/PatternFly-6--Components-Test?node-id=2579-17546',
+  {
+    props: {
       // string
       menuItemText: figma.string('Menu item Text'),
 
@@ -74,27 +107,28 @@ figma.connect(
         true: <CodeBranchIcon />,
         false: undefined
       }),
-      isDanger: figma.enum('Menu Item', { Danger: true }),
-      hasCheckbox: figma.enum('Menu Item', { 'Checkbox select': true }),
-      isSelected: figma.enum('State', { Selected: true }),
-      isDisabled: figma.enum('State', { Disabled: true }),
 
-      itemId: figma.enum('Menu Item', {
-        'Option Select': 'itemId={0}',
-        'With external link on hover': 'itemId={0}'
+      isDrilldown: figma.enum('Menu Item', {
+        'Drill Down - Breadcrumb': <DrilldownMenu />,
+        'Drill Down - Start': <DrilldownMenu />,
+        'Drill Down - Return': <DrilldownMenu />
       }),
 
+      hasCheckbox: figma.enum('Menu Item', { 'Checkbox select': true }),
+      isDanger: figma.enum('Menu Item', { Danger: true }),
+      isSelected: figma.enum('State', { Selected: true }),
+      isDisabled: figma.enum('State', { Disabled: true }),
       withExternalLink: figma.enum('Menu Item', {
         'With external link on hover': { isExternalLink: true, to: '#', itemId: 0 },
 
         // reset conditional values
-        Basic: { ...resetProps },
-        'Checkbox select': { ...resetProps },
-        'Option Select': { ...resetProps },
-        'Drill Down - Breadcrumb': { ...resetProps },
-        'Drill Down - Start': { ...resetProps },
-        'Drill Down - Return': { ...resetProps },
-        Danger: { ...resetProps }
+        Basic: { isExternalLink: false, to: undefined },
+        'Checkbox select': { isExternalLink: false, to: undefined },
+        'Option Select': { isExternalLink: false, to: undefined },
+        'Drill Down - Breadcrumb': { isExternalLink: false, to: undefined },
+        'Drill Down - Start': { isExternalLink: false, to: undefined },
+        'Drill Down - Return': { isExternalLink: false, to: undefined },
+        Danger: { isExternalLink: false, to: undefined }
       })
     },
     example: (props) => (
@@ -107,7 +141,8 @@ figma.connect(
         isDanger={props.isDanger}
         isDisabled={props.isDisabled}
         isSelected={props.isSelected}
-        itemId={props.itemId}
+        itemId="<item-id>"
+        drilldownMenu={props.isDrilldown}
         // description={props.description} // TODO: DESIGN: File bug with Figma CC. The description prop is not working as expected, I assume is due to api, string manipulation.
         description="This is a description."
       >
